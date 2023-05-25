9b97b855ec632c2b70011cc9682cf248d8b92fe9
Muhammad Muneeb <muhammad.muneeb@qenta.com>
2023-05-25 15:45:18 +0500

    deploy code

---
+++ test/login.js
@@ -0,0 +1,13 @@
+describe('duckduckgo example', function() {
+    it('Search Nightwatch.js and check results', function(browser) {
+      browser
+        .navigateTo('https://portal.dev.gmint.io/#/')
+        .waitForElementVisible('input[type="text"]')
+        .sendKeys('input[type="text"]', ['rgsctest+xyzr@gmail.com'])
+        .sendKeys('input[type="password"]', ['@Welcome1234'])
+        .click('*[type="submit"]')
+        .pause(5000)
+        .assert.visible('h1')
+        .assert.textContains('h1', 'Dashboard');
+    }); 
+  });
