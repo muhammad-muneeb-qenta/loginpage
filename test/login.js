935016632a6d0db4b2d2a0abb5f09ed454e7d9d0
Muhammad Muneeb <muhammad.muneeb@qenta.com>
2023-05-25 17:15:08 +0500

    remove

--- test/login.js
+++ test/login.js
@@ -8,7 +8,6 @@ describe('duckduckgo example', function() {
         .click('*[type="submit"]')
         .pause(5000)
         .assert.visible('h1')
-        .assert.textContains('h3', 'Dashboard')
         .assert.textContains('h2', 'Dashboard')
         .assert.textContains('h1', 'Dashboard');
     }); 
