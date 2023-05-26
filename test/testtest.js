09b24e9f883d2a543bba136e1201986db4cd934e
Muhammad Muneeb <muhammad.muneeb@qenta.com>
2023-05-26 16:58:12 +0500

    test

--- test/testtest.js
+++ test/testtest.js
@@ -4,13 +4,13 @@ module.exports = {
         .url('https://www.google.com')
         .waitForElementVisible('body', 1000)
         .assert.title('Google')
-        .assert.visible('input[type=text]')
-        .setValue('input[type=text]', 'rembrandt van rijn')
-        .waitForElementVisible('button[name=btnG]', 1000)
-        .click('button[name=btnG]')
+        //.assert.visible('input[type=text]')
+        .setValue('#APjFqb', 'rembrandt van rijn')
+        .waitForElementVisible('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b', 1000)
+        .click('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b')
         .pause(1000)
         .assert.containsText('ol#rso li:first-child',
           'Rembrandt - Wikipedia')
         .end();
     }
-  };
+};
